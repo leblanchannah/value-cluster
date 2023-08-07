92f910a835bf6e4b4da47f07cfb27ce56c241783
leblanchannah <leblanchannah95@gmail.com>
2023-08-07 16:28:14 -0400

    refactoring, changing layout again

--- src/app.py
+++ src/app.py
@@ -3,6 +3,9 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 
+# product data, aggregated to single row per product  
+df = pd.read_csv('../data/agg_prod_data.csv')
+
 # Initialize the Dash app
 app = Dash(
     __name__,
@@ -28,7 +31,47 @@ sidebar_text = html.P([
         html.Br(),
 ])
 
+sorting_dropdown = dcc.Dropdown(
+                        options=[
+                            {'label':'Best mini compared to full size','value':'ratio_mini_lt_full'},
+                            {'label':'Best full compared to mini size','value':'ratio_full_lt_mini'},
+                            {'label':'Best value mini size','value':'unit_price_mini'},
+                            {'label':'Best value full size','value':'unit_price_full'},
+                        ],
+                        value='ratio_mini_lt_full',
+                        id='sorting_dropdown'
+                    )
+
+brand_filter_global = dcc.Dropdown(
+                        #options = [x for x in df.brand_name.unique()]
+                    )
+product_category_l0_global = dcc.Dropdown(
+                                options = [x for x in df.lvl_0_cat.unique() if x!=' ' and x!='Mini Size' and x!='Men'],
+                                id='category_l0'
+                            )
+
+@callback(
+    Output('scatter_products', 'figure'),
+    Input('category_l0', 'value'))
+def update_product_unit_price_v_size_scatter(value):
+    if not value:
+        return product_unit_price_v_size_scatter(df)
+    return product_unit_price_v_size_scatter(df[df['lvl_0_cat']==value])
+
 ##### Plotly figures and callbacks
+def product_unit_price_v_size_scatter(df):
+    fig = px.scatter(
+                    df,
+                    x='amount_adj',
+                    y='price',
+                    color='swatch_group',
+                    hover_data=['brand_name', 'product_name'],
+                    labels={ # replaces default labels by column name
+                        'amount_adj': "Product size (oz.)",  'price': "Price ($)", "swatch_group": "Product category"
+                    },
+                    template="simple_white"
+    )
+    return fig
 
 
 @callback(
@@ -38,13 +81,11 @@ def update_unit_price_pair_plot(value):
     return unit_price_pair_plot(get_unit_price_comparison_data(df, value))
 
 
-
 def unit_price_pair_plot(df):
     '''
     Returns:
         plotly figure
     '''
-    
     fig = px.line(
                 df,
                 y="value",
@@ -84,6 +125,7 @@ def unit_price_pair_plot(df):
     fig.update_traces(line=dict(width=3))
     return fig 
 
+
 def basic_df_sort(df, col, asc=True, limit=10):
     return df.sort_values(by=col, ascending=asc).head(limit)
 
@@ -102,10 +144,6 @@ def sort_product_comparison_data(df, dropdown_value, limit=10):
     else:
         return df
     
-
-##### Data
-# product data, aggregated to single row per product  
-df = pd.read_csv('../data/agg_prod_data.csv')
 
 def get_unit_price_comparison_data(df, sorting_value='ratio_mini_lt_full'):
     # for each product, compare all mini size to standard using cross join
@@ -132,188 +170,59 @@ def get_unit_price_comparison_data(df, sorting_value='ratio_mini_lt_full'):
     df_compare['display_name'] = df_compare['brand_name']+", "+df_compare['lvl_2_cat'].str.lower()
     return df_compare
 
-
 ###### App 
 app.layout = dbc.Container([
-    html.Br(),
-    dbc.Row([
-        # side panel col, with title, description etc 
-        dbc.Col([
-            dbc.Card(
-                dbc.CardBody([
-                    html.H3("Title placeholder"),
-                    sidebar_text,
-                    html.H4("Sort by:"),
-                    dcc.Dropdown(
-                        options=[
-                            {'label':'Best mini compared to full size','value':'ratio_mini_lt_full'},
-                            {'label':'Best full compared to mini size','value':'ratio_full_lt_mini'},
-                            {'label':'Best value mini size','value':'unit_price_mini'},
-                            {'label':'Best value full size','value':'unit_price_full'},
-                        ],
-                        value='ratio_mini_lt_full',
-                        id='sorting_dropdown'
-                    )
-                ])
-            )
-        ], width=3),
-        # data viz col
-        dbc.Col([
-            # explanatory
-            dbc.Row([
-                # pair plot (mini vs standard)
-                dbc.Col([
-                    dbc.Card(
-                        dbc.CardBody([
-                            dcc.Graph(
-                                id='size_line_plot',
-                                figure=unit_price_pair_plot(get_unit_price_comparison_data(df))
-                            )
-                        ])
-                    )
-                ], width=8),
-                # placeholder for now...
-                dbc.Col([
-                    dbc.Card(dbc.CardBody([]))
-                ], width=4),
-                
-            ]),
-            html.Br(),
-            # exploratory
-            dbc.Row([
-                dbc.Col([
-                    dbc.Card(dbc.CardBody([]))
-                ], width=12)
-            ])
-        ], width=9)
-    ]),
-    html.Br()
+        html.Br(),
+        dbc.Row([
+            # side panel col, with title, description etc 
+            dbc.Col([
+                dbc.Card(
+                    dbc.CardBody([
+                        html.H3("Title placeholder"),
+                        sidebar_text,
+                        html.H4("Sort by:"),
+                        sorting_dropdown,
+                        html.Br(),
+                        html.H4("Filter:"),
+                        product_category_l0_global
+                    ])
+                )
+            ], width=3),
+            # data viz col
+            dbc.Col([
+                # explanatory
+                dbc.Row([
+                    # pair plot (mini vs standard)
+                    dbc.Col([
+                        dbc.Card(
+                            dbc.CardBody([
+                                dcc.Graph(
+                                    id='size_line_plot',
+                                    figure=unit_price_pair_plot(get_unit_price_comparison_data(df))
+                                )
+                            ])
+                        )
+                    ], width=6),
+                    # placeholder for now...
+                    dbc.Col([
+                        dbc.Card(
+                            dbc.CardBody([
+                                dcc.Graph(
+                                    id='scatter_products',
+                                    figure=product_unit_price_v_size_scatter(df)
+                                )
+
+                            ])
+                        )
+                    ], width=6),
+                    
+                ]),
+                html.Br()
+            ], width=9)
+        ]),
+        html.Br(),
+        html.Br()
 ], fluid=True)
-
-
-
-
-# df = pd.read_csv('../data/agg_prod_data.csv')
-
- 
-# # for each product, compare all mini size to standard using cross join
-# df_compare = df[df['swatch_group']=='mini size'].merge(
-#     df[df['swatch_group']=='standard size'],
-#     on=['product_id','product_name','brand_name'],
-#     suffixes=('_mini','_standard')
-# )
-# # only calculate ratio in one direction 
-# df_compare = df_compare[df_compare['amount_adj_mini']<df_compare['amount_adj_standard']]
-# # if ratio < 1, mini is better value per oz, if ratio > 1, standard is better value
-# df_compare['mini_to_standard_ratio'] = df_compare['unit_price_mini'] / df_compare['unit_price_standard']
-
-# fig_hist = px.histogram(df_compare, x="mini_to_standard_ratio")
-# # sort by ratio, best value mini
-# df_compare = filter_product_comparison_data(df_compare, 'mini_to_standard_ratio', asc=True, lim=10)
-# df_compare = df_compare.reset_index()
-# df_compare = df_compare.melt(['product_id','brand_name','product_name','index','amount_adj_mini'])
-# df_compare = df_compare[df_compare['variable'].isin(['unit_price_mini','unit_price_standard'])]
-# print(df_compare)
-
-# # add tooltip
-# # fix axis labels and ticks
-# # fix spacing
-# # fix line labels
-# fig = px.line(
-#                 df_compare,
-#                 y="value",
-#                 x="variable",
-#                 color="index",
-#                 template="simple_white",
-#                 markers=True
-#                 )
-
-# fig.update_xaxes({'autorange': False})
-
-# dropdown_options = {x:x for x in df['lvl_0_cat'].unique() if x!=' '}
-
-
-# # Initialize the Dash app
-# app = Dash(__name__, external_stylesheets=[dbc.themes.LUX], suppress_callback_exceptions=True)
-# # Define the layout of the app\
-# app.layout = html.Div([
-#     # Header section
-#     dbc.Row([
-#         html.H1('Sephora Savings Visualizer'),
-#         html.H3('subtitle.'),
-#     ], style={'textAlign': 'center', 'padding': '20px', 'backgroundColor': '#f2f2f2'}),
-#     dbc.Row([
-#         dbc.Row([
-#             dbc.Col([
-#                 dcc.Input(id='range', type='number', min=5, max=20, step=5)
-#             ], width=3)
-#         ]),
-#         dbc.Row([
-#             dbc.Col([
-#                 html.Div([
-#                     dcc.Graph(
-#                         id='pt_plot',
-#                         figure=fig)
-#                     ]),
-#                 ], width=6),
-#             dbc.Col([
-#                 html.Div([
-#                     dcc.Graph(
-#                         id='ratio_unit_price_hist',
-#                         figure=fig_hist
-#                     )
-#                 ]),
-#             ], width=6)
-#         ])
-#     ]),
-        
-    
-#     # First div with the scatterplot and filters
-#     dbc.Row([
-#         dbc.Col([
-#         dcc.Dropdown(
-#             options=dropdown_options,
-#             value=next(iter(dropdown_options)),
-#             id='crossfilter-dataframe',
-#             ),
-#         dcc.Graph(
-#             id='scatterplot',
-#             figure=px.scatter(
-#                 df,
-#                 x='amount_a',
-#                 y='price',
-#                 color='swatch_group',
-#                 hover_data=['brand_name', 'product_name'],
-#                 labels={ # replaces default labels by column name
-#                     'amount_a': "Product Size (oz*)",  'price': "Price (CAD)", "swatch_group": "Size Category"
-#                 },
-#                 template="simple_white"
-#             ),
-#         ),
-#         # Add filters or any other controls here
-#         # For example: dcc.Dropdown, dcc.RangeSlider, etc.
-#     ], style={'padding': '20px'})])
-
-# ])
-# @callback(
-#     Output('scatterplot', 'figure'),
-#     Input('crossfilter-dataframe', 'value'))
-# def update_graph(value):
-#     dff = df[df['lvl_0_cat'] == value]
-
-    
-#     fig = px.scatter(
-#         dff,
-#         x='amount_a',
-#         y='price',
-#         color='swatch_group',
-#         hover_data=['brand_name', 'product_name'],
-#         labels={ # replaces default labels by column name
-#             'amount_a': "Product Size (oz.)*",  'price': "Price (CAD)", "swatch_group": "Size Category"
-#         },
-#         template="simple_white")
-#     return fig
-
 
 # Run the app
 if __name__ == '__main__':
