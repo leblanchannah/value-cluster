5485b6211214bd3d380fc2c4110e517c7d365a30
leblanchannah <leblanchannah95@gmail.com>
2024-12-28 23:14:40 -0500

    moved old product page scraping functions to new class, testing later

--- src/webscraper.py
+++ src/webscraper.py
@@ -210,6 +210,9 @@ class BrandListScraper:
 
 class ProductScraper:
 
+    def __init__(self, driver):
+        self.driver = driver
+
     @staticmethod
     def get_product_data_api(product_id):
 
@@ -229,212 +232,141 @@ class ProductScraper:
         response = session.get(request_url, headers=headers)
         return response.json()
 
+    def get_product_data_scrape(self, url):
+        #TODO not tested since update to using api for product details
+        product = {}
+        self.driver.get(url)
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        try:
+            h1 = soup.h1.text
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return product
+        if h1=='Sorry, this product is not available.' or h1=='Sorry! The page you’re looking for cannot be found.' or h1=="Search Results":
+            product['error'] = "Product not available"  
+        else:
+            product["product_name"] = self._get_product_name(soup)
+            product["brand_name"] = self._get_brand_name(soup)
+            product["options"] = self._get_product_buttons(self.driver)
+            product["rating"], product["product_reviews"] = self._get_rating_data(soup)
+            product["ingredients"] = get_ingredients(soup)
+            product["n_loves"] = self._get_num_loves(soup)
+            product["categories"] = self._get_breadcrumb_categories(soup)
+        return product
 
-
-
-def parse_url_info(url):
-    """
-    Args:
-    Returns:
-    """
-    url_parsed = urlsplit(url)
-    query = url_parsed.query
-    path = url_parsed.path
-    params = parse_qs(query)
-    return path, params['skuId'][0], params
-
-
-def drop_duplicate_product_urls(brand_data: pd.DataFrame):
-    """
-    """
-    brand_data = pd.json_normalize(brand_data)
-    brand_data = brand_data.explode('products')
-    brand_data = brand_data.reset_index()
-    brand_data = brand_data[brand_data['products'].notnull()]
-    brand_data['url_path'], brand_data['sku'], brand_data['url_params'] = brand_data['products'].apply(parse_url_info).str
-    brand_data = brand_data.drop_duplicates(subset=['name','link','url_path', 'sku'], keep='last')
-    return brand_data
-    
-
-def get_sku(soup) -> str:
-    """
-    Returns sku code from product page in format 'Item #######'
-    SKU can also be found in product URL 
-    """
-    try:
+    def _get_sku(self, soup):
+        #TODO not tested since update to using api for product details
+        """
+        Returns sku code from product page in format 'Item #######'
+        SKU can also be found in product URL 
+        """
         sku_element = soup.find('p', attrs={'data-at': 'item-sku'})
-        if sku_element is not None:
-            return sku_element.text
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-    
-    return "No SKU Found"
-
-
-def get_breadcrumb_categories(soup) -> List:
-    """
-    Returns list of categorical values used to describe product in header of product page
-        for ex. ['Skincare','Moisturizer',...] at time of web scraping there are max of three levels in breadcrumbs
-    """
-    try:
+        if sku_element:
+            return sku_element.text.strip() 
+        
+    def _get_breadcrumb_categories(self, soup) -> List:
+        #TODO not tested since update to using api for product details
+        """
+        Returns list of categorical values used to describe product in header of product page
+            for ex. ['Skincare','Moisturizer',...] at time of web scraping there are max of three levels in breadcrumbs
+        """
         return [x.text for x in soup.find('nav', attrs={'data-comp':"ProductBreadCrumbs BreadCrumbs BreadCrumbs "}).findAll('li')]
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-   
-
-def get_brand_name(soup) -> str:
-    """
-    Returns product brand name from product page
-    """
-    try:
+    
+    def _get_brand_name(self, soup) -> str:
+        #TODO not tested since update to using api for product details
+        """
+        Returns product brand name from product page
+        """
         a_tag = soup.find("a", attrs={'data-at':"brand_name"})
-        if a_tag is not None:
+        if a_tag:
             return a_tag.text
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-    return "Unknown Brand"
-
        
-def get_product_name(soup) -> str:
-    """
-    Returns product name as written on product page
-    """
-    try:
+    def _get_product_name(self, soup) -> str:
+        #TODO not tested since update to using api for product details
+        """
+        Returns product name as written on product page
+        """
         span_tag = soup.find("span", attrs={'data-at': "product_name"})
-        if span_tag is not None:
+        if span_tag:
             return span_tag.text
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-    return "Unknown Product Name"
 
-
-def get_num_loves(soup) -> str:
-    """
-    Returns number of 'love' votes for product
-        loves seem to be used to track product frequently repurchased
-    """
-    try:
+    def _get_num_loves(self, soup) -> str:
+        #TODO not tested since update to using api for product details
+        """
+        Returns number of 'love' votes for product
+            loves seem to be used to track product frequently repurchased
+        """
         span_tag = soup.find("div", attrs={"data-comp": "LovesCount "}).span
-        if span_tag is not None:
+        if span_tag:
             return span_tag.text
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-    return "Unknown Loves Count"
 
-
-def get_product_flag_label(driver) -> str:
-    try:
-        flag_label = driver.find_element(By.XPATH, "//span[@data-at='product_flag_label']")
-        return flag_label.text
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-
-
-def get_ingredients(soup) -> str:
-    """
-    Returns full ingredient list as a blob of text
-    """
-    try:
+    def _get_product_flag_label(self) -> str:
+        #TODO not tested since update to using api for product details
+        flag_label = self.driver.find_element(By.XPATH, "//span[@data-at='product_flag_label']")
+        if flag_label:
+            return flag_label.text
+        
+    def _get_ingredients(self, soup) -> str:
+        #TODO not tested since update to using api for product details
+        """
+        Returns full ingredient list as a blob of text
+        """
         div_ig = soup.find("div", {"id": "ingredients"})
-        if div_ig is not None:
+        if div_ig:
             return div_ig.text
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-    return "Unknown Ingredients"
 
-
-def get_rating_data(soup) -> Tuple[str, str]:
-    """
-        Sephora product page displays a 1-5 bar histogram of votes but it is difficult to retrieve the histogram data
-        ******might be able to figure this out later
-        Returns star rating and number of reviews as tuple
-    """
-    try:
+    def _get_rating_data(self, soup) -> Tuple[str, str]:
+        #TODO not tested since update to using api for product details
+        """
+            Sephora product page displays a 1-5 bar histogram of votes but it is difficult to retrieve the histogram data
+            ******might be able to figure this out later
+            Returns star rating and number of reviews as tuple
+        """
         rr_container = soup.find("a", {"href": "#ratings-reviews-container"})
-        if rr_container is not None:
+        if rr_container:
             star_rating = rr_container.find('span', attrs={'data-at': 'star_rating_style'})['style']
             num_reviews = rr_container.text
             return star_rating, num_reviews
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-    return "", ""
-    
 
-def get_product_buttons(driver, click_delay=CLICK_DELAY) -> Dict:
-    """
-    Products can have size and colour variations on product pages. Each product option available must be clicked
-    on product page to update price.
-    The data produced by this function are messy because product volume formats can look very different depending on brand.
-    Additionally, this function needs to be improved to capture both original and sale prices if a product is on sale.
+    def _get_product_buttons(self, click_delay=CLICK_DELAY) -> Dict:
+        #TODO not tested since update to using api for product details
+        """
+        Products can have size and colour variations on product pages. Each product option available must be clicked
+        on product page to update price.
+        The data produced by this function are messy because product volume formats can look very different depending on brand.
+        Additionally, this function needs to be improved to capture both original and sale prices if a product is on sale.
 
-    """
-    product_options = []
-    # swatch group shows product options within category
-    for swatch_grp in driver.find_elements(By.XPATH, "//div[@data-comp='SwatchGroup ']"):
-        buttons = swatch_grp.find_elements(By.TAG_NAME, "button")
-        for button in buttons:
-            time.sleep(click_delay)
-            try:
-                button.click()
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                print('reached non-clickable web element')
-                return product_options
-            product_info = {}
-            product_info['swatch_group'] = swatch_grp.find_element(By.TAG_NAME, "p").text
-            product_info["flag_label"] = get_product_flag_label(driver)
-            try:
-                product_info['size'] = driver.find_element(By.XPATH, "//span[@data-at='sku_size_label']").text
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                product_info['size'] = None
-            try:
-                product_info['name'] = [x.text for x in driver.find_elements_by_xpath("//div[@data-at='sku_name_label']//span")]
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                product_info['name'] = None
-            # if item is on sale, there will be two b tags, first is sale, second is original price
-            product_info['price'] = [b.text for b in driver.find_elements(By.XPATH, "//p[@data-comp='Price ']//b")]
-            product_info['sku'] = driver.find_element(By.XPATH, "//p[@data-at='item_sku']").text
-            product_options.append(product_info)
-    return product_options
-
-def get_product_page(product_url):
-    """
-    need to update this go just take single product url
-    need to add wrapper function
-    """
-    url = BASE_URL+product_url
-    print(url)
-    time.sleep(CRAWL_DELAY)
-    product = {}
-    product["url"] = url
-    product["error"] = ""
-    product['scrape_timestamp'] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-    # get class names of buttons and grab prices with selenium 
-    driver = webdriver.Chrome(options=options)# executable_path=DRIVER_PATH)
-    driver.get(url)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    try:
-        h1 = soup.h1.text
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        driver.quit()
-        product["error"] = e
-        return product
-    if h1=='Sorry, this product is not available.' or h1=='Sorry! The page you’re looking for cannot be found.' or h1=="Search Results":
-        product['error'] = "Product not available"  
-    else:
-        product["product_name"] = get_product_name(soup)
-        product["brand_name"] = get_brand_name(soup)
-        product["options"] = get_product_buttons(driver)
-        product["rating"], product["product_reviews"] = get_rating_data(soup)
-        product["ingredients"] = get_ingredients(soup)
-        product["n_loves"] = get_num_loves(soup)
-        product["categories"] = get_breadcrumb_categories(soup)
-        
-    driver.quit()
-    return product 
+        """
+        product_options = []
+        # swatch group shows product options within category
+        for swatch_grp in self.driver.find_elements(By.XPATH, "//div[@data-comp='SwatchGroup ']"):
+            buttons = swatch_grp.find_elements(By.TAG_NAME, "button")
+            for button in buttons:
+                time.sleep(click_delay)
+                try:
+                    button.click()
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+                    print('reached non-clickable web element')
+                    return product_options
+                product_info = {}
+                product_info['swatch_group'] = swatch_grp.find_element(By.TAG_NAME, "p").text
+                product_info["flag_label"] = self._get_product_flag_label()
+                try:
+                    product_info['size'] = self.driver.find_element(By.XPATH, "//span[@data-at='sku_size_label']").text
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+                    product_info['size'] = None
+                try:
+                    product_info['name'] = [x.text for x in self.driver.find_elements_by_xpath("//div[@data-at='sku_name_label']//span")]
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+                    product_info['name'] = None
+                # if item is on sale, there will be two b tags, first is sale, second is original price
+                product_info['price'] = [b.text for b in self.driver.find_elements(By.XPATH, "//p[@data-comp='Price ']//b")]
+                product_info['sku'] = self.driver.find_element(By.XPATH, "//p[@data-at='item_sku']").text
+                product_options.append(product_info)
+        return product_options
 
 
 if __name__ == "__main__":
